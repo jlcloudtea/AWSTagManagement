d8185a1cc224e31d7bde89a5fe76d0aa25deeaef
jlcloudtea <114410941+jlcloudtea@users.noreply.github.com>
2023-09-05 08:51:16 +0930

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+PS3='Please enter your choice or press 3 to quit: '
+options=("Create Tag Management Stack" "Delete Tag Management Stack" "Quit")
+select opt in "${options[@]}"
+do
+  case $opt in
+	"Create Tag Management Stack")
+	  echo "you chose choice 1"
+	  echo '-------------------------------------------------------------'
+	  echo ' 	Please wait 2-5 mins until new prompt message...           '
+	  echo '-------------------------------------------------------------'
+	  aws cloudformation create-stack --stack-name TagMan --template-body "file://TagMan.json" >/dev/null 2>&1
+	  aws cloudformation wait stack-create-complete --stack-name TagMan
+ 	  echo '-------------------------------------------------------------'
+	  echo '	Setup Completed You can start the assessment tasks          '
+	  echo '-------------------------------------------------------------'
+	  echo "Below is the related information for your reference"
+	  break
+	  ;;
+	"Delete Tag Management Stack")
+	  aws cloudformation delete-stack --stack-name TagMan
+	  echo '-------------------------------------------------------------'
+	  echo '	Deleting Tag Management Stack may takes 2-5 mins    '
+	  echo '-------------------------------------------------------------'
+	  break
+	  ;;
+	"Quit")
+	  break
+	  ;;
